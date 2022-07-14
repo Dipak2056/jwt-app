626c3ec94685bc0a2ed0dc84771b4e8a6e1d203e
Dipak 2056 <paudelsantosh508@gmail.com>
2022-07-14 21:42:11 +1000

    markup of login and register page done

--- jwt-frontend/src/pages/Register.jsx
+++ jwt-frontend/src/pages/Register.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {
+    e.prevendDefault();
+  };
+  return (
+    <div className="container">
+      <h2>Register</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Password</label>
+          <input
+            type="email"
+            name="password"
+            placeholder="Password"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default Register;
