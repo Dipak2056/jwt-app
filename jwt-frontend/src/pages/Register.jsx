874c087ceb2ad58a2d51635c6d520637b8b483f0
Dipak 2056 <paudelsantosh508@gmail.com>
2022-07-14 23:33:32 +1000

    navigated to secret page on successfull registering

--- jwt-frontend/src/pages/Register.jsx
+++ jwt-frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
 function Register() {
+  const navigate = useNavigate();
   const [values, setValues] = useState({
     email: "",
     password: "",
@@ -31,6 +32,7 @@ function Register() {
           if (email) generateError(email);
           else if (password) generateError(password);
         } else {
+          navigate("/");
         }
       }
     } catch (error) {
