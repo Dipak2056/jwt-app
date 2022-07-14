5a487295e67d9bf9a0828813bf9d25771b1d52c2
Dipak 2056 <paudelsantosh508@gmail.com>
2022-07-14 21:22:49 +1000

    server and the frontend base-setup done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,28 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
 const app = express();
 
-app.listen(4000, () => {
-  console.log("server started at 4000");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("server started at PORT" + PORT);
 });
+mongoose
+  .connect("mongodb://localhost:27017/jwt", {})
+  .then(() => {
+    console.log("DB connection successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    method: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(express.json());
